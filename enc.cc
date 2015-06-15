hghwng/O_O

commit 41cd6b652d4f704bbdd9d5fcf72b24e03558be8d

Remove redundant plus operators.

enc.cc
@@ -34,25 +34,28 @@ int encode_by_sum(size_t v, string &ret) {
   while (v) {
     if (num_parts++) s += " o_O ";
 
-    auto r = rand() % 3;
-    switch (r) {
-      case 0: // 1
-        s += "_";
-        v -= 1;
-        break;
-      case 1: // 2
-        if (v < 2) break;
-        s += "__";
-        v -= 2;
-        break;
-      case 2: // 4
-        if (v < 4) break;
-        s += "___";
-        v -= 4;
-        break;
+    for (;;) {
+      auto r = rand() % 3;
+      switch (r) {
+        case 0: // 1
+          s += "_";
+          v -= 1;
+          goto exit;
+        case 1: // 2
+          if (v < 2) break;
+          s += "__";
+          v -= 2;
+          goto exit;
+        case 2: // 4
+          if (v < 4) break;
+          s += "___";
+          v -= 4;
+          goto exit;
+      }
     }
   }
 
+exit:
   if (num_parts == 1) {
     ret = s;
   } else {

commit 58bc70de78d4558b0ec935a627040cbc9f5856cc

Fix bug caused by 41cd6b652d4f704bbdd9d5fcf72b24e03558be8d

enc.cc
@@ -34,25 +34,28 @@ int encode_by_sum(size_t v, string &ret) {
   while (v) {
     if (num_parts++) s += " o_O ";
 
-    auto r = rand() % 3;
-    switch (r) {
-      case 0: // 1
-        s += "_";
-        v -= 1;
-        break;
-      case 1: // 2
-        if (v < 2) break;
-        s += "__";
-        v -= 2;
-        break;
-      case 2: // 4
-        if (v < 4) break;
-        s += "___";
-        v -= 4;
-        break;
+    for (;;) {
+      auto r = rand() % 3;
+      switch (r) {
+        case 0: // 1
+          s += "_";
+          v -= 1;
+          goto exit;
+        case 1: // 2
+          if (v < 2) break;
+          s += "__";
+          v -= 2;
+          goto exit;
+        case 2: // 4
+          if (v < 4) break;
+          s += "___";
+          v -= 4;
+          goto exit;
+      }
     }
   }
 
+exit:
   if (num_parts == 1) {
     ret = s;
   } else {